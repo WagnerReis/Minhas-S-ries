237bdfd307dead32edfe3ff2c0a77b6bfe87b6b6
Wagner Reis <wagnerreis1997@gmail.com>
2019-12-02 15:18:50 -0300

    Crud de series

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
 import React, { useState, useEffect } from 'react'
 import Header from './Header'
 import Generos from './Generos'
-import axios from 'axios'
+import Series from './Series'
+import InfoSerie from './InfoSerie'
+
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom'
 import NovoGenero from './NovoGenero'
 import EditarGenero from './EditarGenero'
+import NovaSerie from './NovaSerie'
 
 const Home = () => {
   return <h1>Home</h1>
 }
 
 function App() {
-  const [data, setData] = useState({})
-  useEffect(() => {
-    axios.get('/api').then(res => {
-      setData(res.data)
-    })
-  }, [])
   return (
     <Router>
       <div>
         <Header />
-        <Route path='/' exact component={Home} />
-        <Route path='/generos' exact component={Generos} />
-        <Route path='/generos/novo' exact component={NovoGenero} />
-        <Route path='/generos/:id' exact component={EditarGenero} />
-        <pre>{JSON.stringify(data)}</pre>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/generos' exact component={Generos} />
+          <Route path='/generos/novo' exact component={NovoGenero} />
+          <Route path='/generos/:id' exact component={EditarGenero} />
+          <Route path='/series' exact component={Series} />
+          <Route path='/series/novo' exact component={NovaSerie} />
+          <Route path='/series/:id' exact component={InfoSerie} />
+        </Switch>
       </div>
     </Router>
   )
--- src/Series.js
+++ src/Series.js
@@ -2,20 +2,20 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 
-const Generos = () => {
+const Series = () => {
     const [data, setData] = useState([])
     useEffect(() => {
         axios
-            .get('/api/genres')
+            .get('/api/series')
             .then(res => {
                 setData(res.data.data)
             })
     }, [])
 
     
-    const deleteGenero = id => {
+    const deleteSerie = id => {
         axios 
-            .delete('/api/genres/' + id)
+            .delete('/api/series/' + id)
             .then(res => {
                 const filtrado = data.filter(item => item.id !== id)
                 setData(filtrado)
@@ -29,8 +29,8 @@ const Generos = () => {
                 <th scope='row'>{record.id}</th>
                 <td>{record.name}</td>
                 <td>
-                    <button className='btn btn-danger' onClick={() => deleteGenero(record.id)}>Remover</button>
-                    <Link to={'/generos/' + record.id} className='btn btn-warning'>Editar</Link>
+                    <button className='btn btn-danger' onClick={() => deleteSerie(record.id)}>Remover</button>
+                    <Link to={'/series/' + record.id} className='btn btn-warning'>Editar</Link>
                 </td>
             </tr>
         )
@@ -39,10 +39,10 @@ const Generos = () => {
     if (data.length === 0) {
         return(
             <div className='container'>
-                <h1>Genêros</h1>
-                <Link to='/generos/novo' className='btn btn-primary'>Novo genêro</Link>
+                <h1>Séries</h1>
+                <Link to='/series/novo' className='btn btn-primary'>Nova Série</Link>
                 <div className='alert alert-warning' role='alert'>
-                    Você não possui genêros criados.
+                    Você não possui séries criadas.
                  </div>
             </div>
         )
@@ -50,8 +50,8 @@ const Generos = () => {
 
     return (
         <div className='container'>
-            <h1>Generos</h1>
-            <Link to='/generos/novo' className='btn btn-primary'>Novo genêro</Link>
+            <h1>Séries</h1>
+            <Link to='/series/novo' className='btn btn-primary'>Nova Série</Link>
             <table className='table table-dark'>
                 <thead>
                     <tr>
@@ -68,4 +68,4 @@ const Generos = () => {
     )
 }
 
-export default Generos
+export default Series
