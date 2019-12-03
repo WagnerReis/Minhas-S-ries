83bc9e5207ca57c7f95ecf0136058355880976d7
Wagner Reis <wagnerreis1997@gmail.com>
2019-12-03 16:46:20 -0300

    Update info series

--- src/InfoSerie.js
+++ src/InfoSerie.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Redirect } from 'react-router-dom'
+import { Badge } from 'reactstrap'
 
 const InfoSerie = ({ match }) => {
-    const [name, setName] = useState('')
+    const [form, setForm] = useState({})
     const [success, setSuccess] = useState(false)
+    const [mode, setMode] = useState('EDIT')
+    const [genres, setGenres] = useState([])
 
     const [data, setData] = useState({})
     useEffect(() => {
@@ -12,8 +15,16 @@ const InfoSerie = ({ match }) => {
             .get('/api/series/' + match.params.id)
             .then(res => {
                 setData(res.data)
+                setForm(res.data)
             })
     }, [match.params.id])
+    useEffect(() => {
+        axios
+            .get('/api/genres')
+            .then(res => {
+                setGenres(res.data.data)
+            })
+    }, [data])
 
     //custom header
     const masterHeader = {
@@ -25,25 +36,33 @@ const InfoSerie = ({ match }) => {
         backgroundRepeat: 'no-repeat'
     }
 
-    const onChange = evt => {
-        setName(evt.target.value)
+    const onChange = field => evt => {
+        setForm({
+            ...form,
+            [field]: evt.target.value
+        })
     }
+    const seleciona = value => () => {
+        setForm({
+            ...form,
+            status: value
+        })
+    }
+
     const save = () => {
         axios
-            .post('/api/series', {
-                name
-            })
+            .put('/api/series/' + match.params.id, form)
             .then(res => {
                 setSuccess(true)
             })
     }
     if (success) {
-        return <Redirect to='/series' />
+        //return <Redirect to='/series' />
     }
     return (
         <div>
             <header style={masterHeader}>
-                <div className='h-100' style={{ background: 'rgba(0,0,0,0.7)'}}>
+                <div className='h-100' style={{ background: 'rgba(0,0,0,0.7)' }}>
                     <div className='h-100 container'>
                         <div className='row h-100 align-items-center'>
                             <div className='col-3'>
@@ -51,22 +70,55 @@ const InfoSerie = ({ match }) => {
                             </div>
                             <div className='col-8'>
                                 <h1 className='font-weight-light text-white'>{data.name}</h1>
+                                <div className='lead text-white'>
+                                    <Badge color='success'>Assitido</Badge>
+                                    <Badge color='warning'>Para assistir</Badge>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </header>
-            <div className='container'>
-                <h1>Nova Série</h1>
-                <pre>{JSON.stringify(data)}</pre>
-                <form>
-                    <div className='form-group'>
-                        <label htmlFor='name'>Nome</label>
-                        <input type='text' value={name} onChange={onChange} className='form-control' id='name' placeholder='Nome da Série' />
-                    </div>
-                    <button type='button' onClick={save} className='btn btn-primary'>Salvar</button>
-                </form>
+            <div>
+                <button className='btn btn-primary' onClick={() => setMode('EDIT')}>Editar</button>
             </div>
+            {
+                mode === 'EDIT' &&
+                <div className='container'>
+                    <h1>Nova Série</h1>
+                    <pre>{JSON.stringify(form)}</pre>
+                    <button className='btn btn-danger' onClick={() => setMode('INFO')}>Cancelar edição</button>
+                    <form>
+                        <div className='form-group'>
+                            <label htmlFor='name'>Nome</label>
+                            <input type='text' value={form.name} onChange={onChange('name')} className='form-control' id='name' placeholder='Nome da Série' />
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor='name'>Comentários</label>
+                            <input type='text' value={form.comments} onChange={onChange('comments')} className='form-control' id='name' placeholder='Comentarios' />
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor='name'>Gênero</label>
+                            <select className='form-control' onChange={onChange('genre_id')}>
+                                {genres.map(genre => <option key={genre.id} value={genre.id} select={genre.id === form.genre}>{genre.name}</option>)}
+                            </select>
+                        </div>
+                        <div className='form-check'>
+                            <input className='form-check-input' type='radio' name='status' id='assistido' value='ASSISTIDO' onClick={seleciona('ASSISTIDO')} />
+                            <label className='form-check-label' htmlFor='assistido'>
+                                Assistido
+                            </label>
+                        </div>
+                        <div className='form-check'>
+                            <input className='form-check-input' type='radio' name='status' id='paraAssistir' value='PARA_ASSISTIR' onClick={seleciona('PARA_ASSISTIR')} />
+                            <label className='form-check-label' htmlFor='paraAssistir'>
+                                Para assistir
+                            </label>
+                        </div>
+                        <button type='button' onClick={save} className='btn btn-primary'>Salvar</button>
+                    </form>
+                </div>
+            }
         </div>
     )
 }
